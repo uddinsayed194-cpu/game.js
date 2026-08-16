ff3fcd9b04065f5196a0f525a8e14a492f5b014c
Arif <uddinsayed194@gmail.com>
2026-08-16 17:37:51 +0600

    Create game.js

---
+++ game.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Arif Football</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div id="game">
+    <div id="scoreboard">
+      <span>ARIF FC</span>
+      <span id="score">0 - 0</span>
+      <span>RIVAL FC</span>
+    </div>
+
+    <canvas id="field"></canvas>
+
+    <div id="message">Drag the ball to pass or shoot!</div>
+  </div>
+
+  <script src="game.js"></script>
+
+</body>
+</html>
